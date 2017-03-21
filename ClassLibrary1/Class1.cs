db9871b1ba56583a1a94ea67bf3900f2de94fd42
Matthew Leibowitz <mattleibow@live.com>
2017-03-21 21:47:52 +0200

    Added the test case

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ClassLibrary1
+{
+	public class Class1
+	{
+		public static bool Test(Stream stream)
+		{
+			var reader = XmlReader.Create(stream);
+			return reader.Read();
+		}
+	}
+}
